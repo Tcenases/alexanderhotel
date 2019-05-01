ab32232b860a279bd21861b75cd93ed7963da092
Tcenases <tcenases@gmail.com>
2019-05-01 21:54:23 +0300

    reservation step1 major functionality

--- js/index.js
+++ js/index.js
@@ -5,35 +5,18 @@ function init() {
     document.getElementById("check_button").addEventListener("click", startReservationProcess, false);
 
     function startReservationProcess() {
-        var startDate = document.getElementById("check-in");
-        var endDate = document.getElementById("check-out");
-        var date = new Date;
-        var flag = true;
-
-        if (startDate.value < date.yyyymmdd()) {
-            flag = false;
-            startDate.style.backgroundColor = "rgba(255, 7, 7, 0.2)";
-            startDate.style.border = "2px solid red";
+        window.sessionStorage.flag = "index";
+        window.sessionStorage.startDate = document.getElementById("check-in").value;
+        window.sessionStorage.endDate = document.getElementById("check-out").value;
+        window.sessionStorage.adults = document.getElementById("adults").value;
+        window.sessionStorage.children = document.getElementById("children").value;
+        var url = window.location.href;
+        if (url.indexOf("index.html") > 0) {
+            var newUrl = url.replace("index.html", "reservation.html");
+        }
+        else {
+            newUrl = url + "reservation.html";
         };
-        if (endDate.value <= startDate.value) {
-            flag = false;
-            endDate.style.backgroundColor = "rgba(255, 7, 7, 0.2)";
-            endDate.style.border = "2px solid red";
-        };
-        if (flag) {
-            window.sessionStorage.startDate = startDate.value;
-            window.sessionStorage.endDate = endDate.value;
-            window.sessionStorage.adults = document.getElementById("adults").value;
-            window.sessionStorage.children = document.getElementById("children").value;
-            var url = window.location;
-            if ((String.prototype.indexOf.call(url, "index.html")) > 0) {
-                var newUrl = String.prototype.replace.call(url, "index.html", "reservation.html");
-            }
-            else {
-                newUrl = url.href + "reservation.html";
-            }
-            window.location.assign(newUrl);
-        };
+        window.location.assign(newUrl);
     };
-
-;}
+};
--- js/reservation.js
+++ js/reservation.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", init, false);
+window.addEventListener("load", init);
 
 //method to get date in "yyyy-mm-dd" format
 Date.prototype.yyyymmdd = function() {
@@ -8,13 +8,10 @@ Date.prototype.yyyymmdd = function() {
     return yyyy + mm + dd;
 };
 
-
-
-
-
-
 function init() {
-    setTimeout(scroll, 1000);
+
+
+    setTimeout(scroll, 500);
 
     function scroll() {
         var url = window.location.href;
@@ -23,5 +20,83 @@ function init() {
             url = url.substring(0, index);
         };
         window.location.replace(url + "#reservation-block");
-    }
+    };
+
+    document.getElementById("check-button").addEventListener("click", firstStep);
+
+    var startDate = document.getElementById("check-in");
+    var endDate = document.getElementById("check-out");
+    var adults = document.getElementById("adults");
+    var children = document.getElementById("children");
+    var roomType = document.getElementById("room_type");
+    var flag1;
+    var flag2;
+    var errorMsg1 = document.getElementById("error_msg1");
+    var errorMsg2 = document.getElementById("error_msg2");
+
+    startDate.addEventListener("blur", validateStartDate);
+    endDate.addEventListener("blur", validateEndDate);
+
+    function validateStartDate() {
+        if (startDate.value <= "2019-05-02") {
+            startDate.classList.add("invalid");
+            flag1 = false;
+            errorMsg1.style.display = "inline";
+            validateEndDate();
+        }
+        else {
+            startDate.classList.remove("invalid");
+            flag1 = true;
+            errorMsg1.style.display = "none";
+        }
+    };
+
+    function validateEndDate() {
+        if (endDate.value <= "2019-05-02" || endDate.value <= startDate.value) {
+            endDate.classList.add("invalid");
+            flag2 = false;
+            errorMsg2.style.display = "inline";
+            //validateStartDate();
+        }
+        else {
+            endDate.classList.remove("invalid");
+            flag2 = true;
+            errorMsg2.style.display = "none";
+        }
+    };
+
+
+    if (window.sessionStorage.flag == "index") {
+        firstStep();
+    };
+
+
+    function firstStep() {
+        if (window.sessionStorage.flag == "index") {
+            window.sessionStorage.flag = "";
+            if (window.sessionStorage.startDate) {startDate.value = window.sessionStorage.startDate; validateStartDate()};
+            if (window.sessionStorage.endDate) { endDate.value = window.sessionStorage.endDate; validateEndDate()};
+            if (window.sessionStorage.adults) { adults.value = window.sessionStorage.adults; };
+            if (window.sessionStorage.children) {children.value = window.sessionStorage.children; };
+        };
+
+        if (flag1 && flag2) {
+            window.sessionStorage.startDate = startDate.value;
+            window.sessionStorage.endDate = endDate.value;
+            window.sessionStorage.adults = adults.value;
+            window.sessionStorage.children = children.value;
+            window.sessionStorage.roomType = roomType.value;
+            window.sessionStorage.flag = "reservations";
+            secondStep();
+            //alert("OK");
+        };
+    };
+
+    function secondStep() {
+        var steps = document.getElementsByClassName("step");
+        steps[0].classList.add("completed_step");
+        steps[0].classList.remove("current_step");
+        steps[1].classList.add("current_step");
+    };
+
 };

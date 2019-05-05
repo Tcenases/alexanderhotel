7b345ce67ed002b979c16c2d05805c10134db33b
Tcenases <tcenases@gmail.com>
2019-05-05 21:34:14 +0300

    more roooms added

--- js/JSON-generator.js
+++ js/JSON-generator.js
@@ -62,13 +62,13 @@ hotelRooms.push(room4);
 
 var room5 = {};
 room5.name = null;
-room5.category = "? Room";
+room5.category = "Family Room";
 room5.roomNumber = "105";
-room5.size = "60";
-room5.bedType = "Two beds";
-room5.maxPersons = "4";
+room5.size = "120";
+room5.bedType = "Five beds";
+room5.maxPersons = "5";
 room5.facilities = {};
-room5.price = 403;
+room5.price = 725;
 room5.bookedPeriods = null;
 room5.featured = false;
 room5.image = "images/img_5.jpg";
@@ -77,18 +77,108 @@ hotelRooms.push(room5);
 
 var room6 = {};
 room6.name = null;
-room6.category = "? Room";
+room6.category = "Double Room";
 room6.roomNumber = "106";
-room6.size = "60";
+room6.size = "45";
 room6.bedType = "Two beds";
-room6.maxPersons = "4";
+room6.maxPersons = "2";
 room6.facilities = {};
-room6.price = 676;
+room6.price = 330;
 room6.bookedPeriods = null;
 room6.featured = false;
 room6.image = "images/img_6.jpg";
 
 hotelRooms.push(room6);
+
+var room7 = {};
+room7.name = null;
+room7.category = "Family Room";
+room7.roomNumber = "107";
+room7.size = "125";
+room7.bedType = "Seven beds";
+room7.maxPersons = "7";
+room7.facilities = {};
+room7.price = 1240;
+room7.bookedPeriods = null;
+room7.featured = false;
+room7.image = "images/img_2.jpg";
+
+hotelRooms.push(room7);
+
+var room8 = {};
+room8.name = null;
+room8.category = "Double Room";
+room8.roomNumber = "108";
+room8.size = "40";
+room8.bedType = "Two beds";
+room8.maxPersons = "2";
+room8.facilities = {};
+room8.price = 241;
+room8.bookedPeriods = null;
+room8.featured = false;
+room8.image = "images/img_2.jpg";
+
+hotelRooms.push(room8);
+
+var room9 = {};
+room9.name = null;
+room9.category = "Bachelor Room";
+room9.roomNumber = "109";
+room9.size = "40";
+room9.bedType = "One Bed";
+room9.maxPersons = "1";
+room9.facilities = {};
+room9.price = 158;
+room9.bookedPeriods = null;
+room9.featured = false;
+room9.image = "images/img_3.jpg";
+
+hotelRooms.push(room9);
+
+var room10 = {};
+room10.name = null;
+room10.category = "VIP Room";
+room10.roomNumber = "110";
+room10.size = "140";
+room10.bedType = "Eight Beds";
+room10.maxPersons = "10";
+room10.facilities = {};
+room10.price = 1568;
+room10.bookedPeriods = null;
+room10.featured = false;
+room10.image = "images/img_4.jpg";
+
+hotelRooms.push(room10);
+
+var room11 = {};
+room11.name = null;
+room11.category = "VIP Room";
+room11.roomNumber = "111";
+room11.size = "180";
+room11.bedType = "Ten Beds";
+room11.maxPersons = "14";
+room11.facilities = {};
+room11.price = 2083;
+room11.bookedPeriods = null;
+room11.featured = false;
+room11.image = "images/img_5.jpg";
+
+hotelRooms.push(room11);
+
+var room12 = {};
+room12.name = null;
+room12.category = "Presidental Room";
+room12.roomNumber = "112";
+room12.size = "240";
+room12.bedType = "Six Beds";
+room12.maxPersons = "12";
+room12.facilities = {};
+room12.price = 3500;
+room12.bookedPeriods = null;
+room12.featured = false;
+room12.image = "images/img_6.jpg";
+
+hotelRooms.push(room12);
 
 var data = JSON.stringify(hotelRooms);
 console.log(data);

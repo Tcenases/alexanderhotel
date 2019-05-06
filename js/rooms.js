88c3a43909e44221c77733f1821e5b59991970d7
Tcenases <tcenases@gmail.com>
2019-05-06 21:56:21 +0300

    small rooms/singleRoom changes

--- js/rooms.js
+++ js/rooms.js
@@ -29,19 +29,31 @@ function init() {
     function loadRooms() {
 
         var featuredContainer = document.getElementById("featuredContainer");
-        // var simpleCondainer = document.getElementById("");
+        var simpleContainer = document.getElementById("simpleContainer");
         document.getElementById("loader").style.display = "none";
         var controller = true;
 
-        for (var i = 0; i < data.length; i++) {
+        for (var i = 0; i < data.length; i++) {   
+            var elem = document.createElement("div");  
+            elem.innerHTML = "<img src='" + data[i].image + "' alt='room" + i + "'><div class='room_summary'><h2 class='room_heading'>" + data[i].category + "</h2><p class='specs'><sup>$</sup><span class='price'>" + data[i].price + "</span><sub>/per night</sub></p><p class='specs'><span class='specification'>Adults: </span>" + data[i].maxPersons + "</p><p class='specs'><span class='specification'>Categories: </span>" + data[i].category + "</p><p class='specs'><span class='specification'>Facilities: </span>Closet with hangers, HD flat-screen TV, Telephone</p><p class='specs'><span class='specification'>Size: </span>" + data[i].size + "m<sup>2</sup></p><p class='specs'><span class='specification'>Bed Type: </span>" + data[i].bedType + "</p><button id='" + data[i].roomNumber + "' class='readmore_button'>Read More</button>"
             if (data[i].featured) {
-                var elem = document.createElement("div");
-                controller ? elem.classList.add("room_preview--featured") : elem.classList.add("room_preview--featured-reversed");
-                controller = !controller;
                 elem.classList.add("room_preview--featured")
-                elem.innerHTML = "<img src='" + data[i].image + "' alt='room" + i + "'><div class='room_summary'><h2 class='room_heading'>" + data[i].category + "</h2><p class='specs'><sup>$</sup><span class='price'>" + data[i].price + "</span><sub>/per night</sub></p><p class='specs'><span class='specification'>Adults: </span>" + data[i].maxPersons + "</p><p class='specs'><span class='specification'>Categories: </span>" + data[i].category + "</p><p class='specs'><span class='specification'>Facilities: </span>Closet with hangers, HD flat-screen TV, Telephone</p><p class='specs'><span class='specification'>Size: </span>" + data[i].size + "m<sup>2</sup></p><p class='specs'><span class='specification'>Bed Type: </span>" + data[i].bedType + "</p><button class='readmore_button'>Read More</button>"
+                if (controller) { elem.classList.add("room_preview--featured-reversed") };
+                controller = !controller;
                 featuredContainer.appendChild(elem);
-            };
+            } else {
+                elem.classList.add("room_preview");
+                simpleContainer.appendChild(elem);
+            };      
         };
-    };   
+    };  
+    
+    document.getElementById("rooms_showcase").addEventListener("click", singleRoom);
+
+    function singleRoom(e) {
+        if (e.target.classList.contains("readmore_button")){
+            window.location.assign("singleRoom.html?roomNumber=" + e.target.id);
+        };
+    };
+
 };

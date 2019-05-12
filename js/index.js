a2a7df775bfd6bf205b81476971140f3aa5b6d8e
Tcenases <tcenases@gmail.com>
2019-05-12 16:25:01 +0300

    added slider on the main page

--- js/index.js
+++ js/index.js
@@ -2,7 +2,19 @@ window.addEventListener("load", init, false);
 
 function init() {
 
+    var counter = 0;
+    var slides = document.getElementsByClassName("main_slider");
+    var texts = document.getElementsByClassName("main_block");
+    var bars = document.getElementsByClassName("bar");
+    var arrows = document.getElementsByClassName("arrow");
+    var clicked;
+    var totalSlides = slides.length;
+
     document.getElementById("check_button").addEventListener("click", startReservationProcess);
+    arrows[0].addEventListener("click", function() { counter - 2; if (counter < 0) { counter = totalSlides - 1 }; clicked = Date.now(); mainSlider() });
+    arrows[1].addEventListener("click", function() { clicked = Date.now(); mainSlider() });
+
+    setInterval(autoSlider, 5000);
 
     function startReservationProcess() {
         window.sessionStorage.flag = "index";
@@ -12,4 +24,30 @@ function init() {
         window.sessionStorage.children = document.getElementById("children").value;
         window.location.assign("reservation.html");
     };
+
+    function mainSlider() {
+        
+        var next = (counter + 1) == totalSlides ? 0 : counter + 1;
+
+        slides[counter].style.opacity = 0;
+        slides[next].style.opacity = 1;
+        
+        texts[counter].style.opacity = 0;
+        texts[counter].style.display = "none";
+        texts[next].style.display = "block"; setTimeout(function() {texts[next].style.opacity = 1}, 20);
+
+        bars[counter].classList.remove("active");
+        bars[next].classList.add("active");
+
+        ++counter;
+        if (counter == totalSlides) {
+            counter = 0;
+        };
+
+    };
+
+    function autoSlider() {
+        if (clicked == undefined || Date.now() - clicked > 10000) mainSlider();
+    };
+
 };

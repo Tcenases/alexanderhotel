d28bd169357cfb9608c7d522b354581f84823279
Tcenases <tcenases@gmail.com>
2019-05-12 20:25:29 +0300

    added miniSlider on the main page

--- js/index.js
+++ js/index.js
@@ -15,6 +15,7 @@ function init() {
     arrows[1].addEventListener("click", function() { clicked = Date.now(); mainSlider() });
 
     setInterval(autoSlider, 5000);
+    miniSlider();
 
     function startReservationProcess() {
         window.sessionStorage.flag = "index";
@@ -34,7 +35,7 @@ function init() {
         
         texts[counter].style.opacity = 0;
         texts[counter].style.display = "none";
-        texts[next].style.display = "block"; setTimeout(function() {texts[next].style.opacity = 1}, 20);
+        texts[next].style.display = "block"; setTimeout(function() {texts[next].style.opacity = 1}, 100);
 
         bars[counter].classList.remove("active");
         bars[next].classList.add("active");
@@ -48,6 +49,56 @@ function init() {
 
     function autoSlider() {
         if (clicked == undefined || Date.now() - clicked > 10000) mainSlider();
+    };
+
+    function miniSlider() {
+        var elem =  document.getElementsByClassName("room_preview")[0];
+        var styles = window.getComputedStyle(elem);
+        var width = styles.getPropertyValue("width");
+        var margin = styles.getPropertyValue("margin-right");
+        var slideWidth = +width.substring(0, width.indexOf("px")) + +margin.substring(0, margin.indexOf("px"));
+        var arrowsContainer = document.getElementById("rooms_navigation");
+        arrowsContainer.children[0].addEventListener("click", left);
+        arrowsContainer.children[1].addEventListener("click", right);
+        var containerStyles = window.getComputedStyle(document.getElementsByClassName("container")[0]);
+        var containerWidth = containerStyles.getPropertyValue("width")
+        var slidesAmount = 6 - Math.round(+containerWidth.substring(0, containerWidth.indexOf("px")) / slideWidth);
+        alert(slidesAmount);
+        var position = 0;
+       
+        function right() {
+            if (position < slidesAmount) {
+                ++position
+                elem.parentElement.style.left = "-" + slideWidth * position + "px";
+                arrowsContainer.children[0].style.color = "black";
+                alert(1);
+            };
+            if (position == slidesAmount) arrowsContainer.children[1].style.color = "gray"; 
+        };
+
+        function left() {
+            if (position > 0) {
+                --position
+                elem.parentElement.style.left = "-" + slideWidth * position + "px";
+                arrowsContainer.children[1].style.color = "black"; 
+            };
+            if (position == 0) arrowsContainer.children[0].style.color = "gray"; 
+        };
+
+        function setupSlider() {
+            elem =  document.getElementsByClassName("room_preview")[0];
+            styles = window.getComputedStyle(elem);
+            width = styles.getPropertyValue("width");
+            margin = styles.getPropertyValue("margin-right");
+            slideWidth = +width.substring(0, width.indexOf("px")) + +margin.substring(0, margin.indexOf("px"));
+            containerStyles = window.getComputedStyle(document.getElementsByClassName("container")[0]);
+            containerWidth = containerStyles.getPropertyValue("width")
+            slidesAmount = 6 - Math.round(+containerWidth.substring(0, containerWidth.indexOf("px")) / slideWidth);
+            position = 0;
+        };
+
+        window.addEventListener("resize", setupSlider);
+
     };
 
 };
